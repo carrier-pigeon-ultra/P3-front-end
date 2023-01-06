cdc1e85158647b10cdc755beff6c92ccb59d775e
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-06 12:08:23 -0800

    Added hosted url to envirment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  withCredentials: true,
+  baseUrl: "carrierpigeonultra-env.eba-rpgdxup3.us-east-2.elasticbeanstalk.com:5000",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+  },
 };
