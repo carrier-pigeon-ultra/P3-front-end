9cce25591fb9060db37cd85793f4b92aeaf24870
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-01-09 13:53:52 -0800

    Created Reset Password Page

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  styleUrls: ['./reset-password.component.css'],
 })
 export class ResetPasswordComponent implements OnInit {
+  resetPasswordForm = new FormGroup({
+    email: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // onSubmit(e: any): void {
+  //   e.preventDefault()
+  //   this.resetPasswordForm.value.email || "")
+  //     .subscribe(
+  //       (response) => {
+  //         response
+  //         this.router.navigate(['/login'])
+  //       }
+  //     )
+  // }
+
+  submit() {
+    let userEmail = this.resetPasswordForm.value.email;
   }
-
 }
