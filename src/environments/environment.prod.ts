474b6ce0714e916d317d6851c9ffa1ad24febcaf
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-09 09:27:25 -0800

    Removed 500 from url, added S3 url to allowed origens in headers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   baseUrl: "carrierpigeonultra-env.eba-rpgdxup3.us-east-2.elasticbeanstalk.com",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin': ['http://localhost:4200', "http://codepipeline-us-west-2-791209503483.s3-website-us-west-2.amazonaws.com"], 
   },
 };
