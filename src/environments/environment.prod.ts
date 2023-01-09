e546de679efe19d285ca87537820e437344faeaf
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-09 09:33:50 -0800

    http:// added to baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "carrierpigeonultra-env.eba-rpgdxup3.us-east-2.elasticbeanstalk.com",
+  baseUrl: "http://carrierpigeonultra-env.eba-rpgdxup3.us-east-2.elasticbeanstalk.com",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': ['http://localhost:4200', "http://codepipeline-us-west-2-791209503483.s3-website-us-west-2.amazonaws.com"], 
