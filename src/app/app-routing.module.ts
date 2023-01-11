7b458d6825d8b011e3c65625a94842a97ccc14ed
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-10 18:21:36 -0800

    Route configured for ProfileViewComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PostFeedPageComponent } from './components/post-feed-page/post-feed-page.component';
+import { ProfileViewComponent } from './components/profile-view/profile-view.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SearchUserComponent } from './components/search-user/search-user.component';
 import { ViewPersonalProfileComponent } from './components/view-personal-profile/view-personal-profile.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'post-feed', component: PostFeedPageComponent },
   { path: 'search-bar', component: SearchUserComponent },
-    { path: "my-profile", component: ViewPersonalProfileComponent }
+  { path: "my-profile", component: ViewPersonalProfileComponent },
+  { path: "profile", component: ProfileViewComponent }
 ]
 
 @NgModule({
